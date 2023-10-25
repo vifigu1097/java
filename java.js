09b797efa072bbe24964b642e0d03df3d740d8f8
Linaa Finklea <sky@linaas-mbp.mynetworksettings.com>
2023-10-25 11:30:32 -0400

    add psuedo

--- java.js
+++ java.js
@@ -14,16 +14,28 @@
 //HelloWorld();
 
 // create 4 functions that take in 2 numbers 
+// asking me to create 4 sepreate functions 
+// in these lines (num1, num2) these are parameters 
+// takes in 2 numbers means  num1 and num2  
+
 
 // create a function called add 
 // add should take in 2 numbers 
-    const add = () => {
-    console.log('Did this Work?')
+// , seperates the two numbers > always be detailed 
+// ways to test console.log(num1, num2);
+    const add = (num1, num2) => {
+    return num1 + num2;
 }
-add();
+// console.log(add(5, 4));
+// add(5, 6); this is the argument
 // return number1 + numbers 
 
 
+const multiply = (num1, num2) => {
+    return num1 * num2 ;
+}
+// 
+//multiply(5, 9);
 
 
 
