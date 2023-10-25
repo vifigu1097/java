9469e4178231163b66d1638dd05d0f4a0f9782ae
Linaa Finklea <sky@linaas-mbp.mynetworksettings.com>
2023-10-25 11:28:36 -0400

    add java
    >
    >

---
+++ java.js
@@ -0,0 +1,34 @@
+// contst text - ('Hello World!') ->
+//const text - () -> {
+//    return 'Hello World!'
+//}
+//console.log('helloworld');
+
+//const HelloWorld = () => {
+  //  console.log('Testing');
+//}
+//return  "Hello World!"
+
+//console.log(helloWorld);
+
+//HelloWorld();
+
+// create 4 functions that take in 2 numbers 
+
+// create a function called add 
+// add should take in 2 numbers 
+    const add = () => {
+    console.log('Did this Work?')
+}
+add();
+// return number1 + numbers 
+
+
+
+
+
+
+
+
+
+
